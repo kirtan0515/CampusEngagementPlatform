4d24869065ab9b121df806737b48281fe80bd715
kirtan0515 <kirtan.patel0515#gmail.com>
2024-07-13 12:58:32 -0400

    updated header and footer

--- ConnectNest/frontend/src/components/Header/Header.js
+++ ConnectNest/frontend/src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Header.css';
+import logo from './logo.png';
 
 function Header() {
   const navigate = useNavigate();
@@ -12,12 +13,19 @@ function Header() {
 
   return (
     <header className="header">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/engagements">Engagements</Link>
-        <button onClick={handleLogout}>Logout</button>
-      </nav>
+      <div className="container">
+        <div className="logo-container">
+          <img src={logo} alt="ConnectNest Logo" className="logo" />
+        </div>
+        <nav className="nav">
+          <Link to="/">Home</Link>
+          <Link to="/dashboard">Dashboard</Link>
+          <Link to="/engagements">Engagements</Link>
+          <Link to="/help">Help</Link>
+          <Link to="/faqs">FAQs</Link>
+          <button onClick={handleLogout} className="logout-button">Logout</button>
+        </nav>
+      </div>
     </header>
   );
 }
--- ConnectNest/frontend/src/components/LandingPage/LandingPage.js
+++ ConnectNest/frontend/src/components/LandingPage/LandingPage.js
@@ -10,22 +10,11 @@ const LandingPage = () => {
       <div className="blur-circle2"></div>
 
       <div className="landing-page">
-        <header>
-          <div className="container">
-            <img src={logo} alt="ConnectNest Logo" className="landing-logo" />
-            <ul className="links">
-              <li><Link to="/login" className="nav-button">Login</Link></li>
-              <li><Link to="/signup" className="nav-button">Sign Up</Link></li>
-              <li><Link to="/help" className="nav-button">Help</Link></li>
-              <li><Link to="/faqs" className="nav-button">FAQs</Link></li>
-            </ul>
-          </div>
-        </header>
         <div className="content">
           <div className="container">
             <div className="info">
               <h1>Unite. Engage. Inspire.</h1>
-              <p>Discover a new way to stay connected and engaged with your campus commounity. Wheather you're a student, faculty, or staff, ConnectNest is here to bring everyone together.</p>
+              <p>Discover a new way to stay connected and engaged with your campus community. Whether you're a student, faculty, or staff, ConnectNest is here to bring everyone together.</p>
               <button className="cta-button"><Link to="/signup" className="cta-link">Get Started</Link></button>
             </div>
             <div className="image">
